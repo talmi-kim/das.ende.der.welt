b186613533e621c58b8610266f72c3b977808465
talmi-kim <knv.kimnganvo@gmail.com>
2026-02-28 12:35:07 +0100

    Aktualisieren von script.js

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-const textEl = document.getElementById("text");
-const authorEl = document.getElementById("author");
+document.addEventListener("DOMContentLoaded", function () {
 
-const text = "Komm mit mir bis ans Ende der Welt";
-const authorText = "– Kim, aus der Zukunft.";
+  const textEl = document.getElementById("text");
+  const authorEl = document.getElementById("author");
 
-let i = 0;
+  const text = "Komm mit mir bis ans Ende der Welt";
+  const authorText = "– Kim, aus der Zukunft.";
 
-function typeMainText() {
-  if (i < text.length) {
-    if (text.slice(i, i + 4) === "Ende") {
-      textEl.innerHTML += `<span class="red">Ende</span>`;
-      i += 4;
+  let i = 0;
+
+  function typeMainText() {
+    if (i < text.length) {
+      if (text.slice(i, i + 4) === "Ende") {
+        textEl.innerHTML += `<span class="red">Ende</span>`;
+        i += 4;
+      } else {
+        textEl.innerHTML += text[i];
+        i++;
+      }
+      setTimeout(typeMainText, 100);
     } else {
-      textEl.innerHTML += text[i];
-      i++;
+      setTimeout(typeAuthorText, 600);
     }
-    setTimeout(typeMainText, 100);
-  } else {
-    setTimeout(typeAuthorText, 600);
   }
-}
 
-let j = 0;
-function typeAuthorText() {
-  if (j < authorText.length) {
-    authorEl.textContent += authorText[j];
-    j++;
-    setTimeout(typeAuthorText, 70);
-  } else {
-    
-    authorEl.innerHTML = authorEl.innerHTML.replace(
-      "Zukunft",
-      "<span class='glitch' data-text='Zukunft'>Zukunft</span>"
-    );
+  let j = 0;
+
+  function typeAuthorText() {
+    if (j < authorText.length) {
+      authorEl.textContent += authorText[j];
+      j++;
+      setTimeout(typeAuthorText, 70);
+    } else {
+      authorEl.innerHTML = authorEl.innerHTML.replace(
+        "Zukunft",
+        "<span class='glitch'>Zukunft</span>"
+      );
+    }
   }
-}
 
-typeMainText();
+  typeMainText();
+
+});
